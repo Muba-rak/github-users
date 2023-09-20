6cf753a9f7a2a763b3ff8631f7885a9e4c64ceaf
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-20 10:42:47 +0100

    added pie chart for languages

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -5,11 +5,46 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 import { GithubContext } from "../context/context";
 const Repos = () => {
   const { repos } = useContext(GithubContext);
-  console.log(repos);
+  // console.log(repos);
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) {
+      return total;
+    }
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  console.log(languages);
+  const chartData = [
+    {
+      label: "Venezuela",
+      value: "290",
+    },
+    {
+      label: "Saudi",
+      value: "260",
+    },
+    {
+      label: "Canada",
+      value: "180",
+    },
+  ];
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <ExampleChart />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   );
